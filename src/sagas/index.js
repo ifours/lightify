95cbff1ce886b23f26d4a420df7408865d035c7f
IFours <ilia.sachok@gmail.com>
2017-07-07 12:09:55 +0300

    rename saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 
-import helloSaga from './test';
+import helloSaga from './hello';
 import watchFetchResources from './resources';
 
 // single entry point to start all Sagas at once
